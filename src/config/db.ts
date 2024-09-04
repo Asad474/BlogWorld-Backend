4e6c02af682d3990aa0be920279be4264d2af571
Asad Sarfaraz <asadsarfraz474@gmail.com>
2024-09-04 20:06:06 +0530

    Refactoring database connection

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,22 @@
-import { connect } from "mongoose";
+import mongoose from "mongoose";
 
 export const DBConnection = async() => {
     try {
         const url = process.env.MONGODB_URL as string;
-        await connect(url);
-        console.log('Database Connected.');
+
+        mongoose.connection.on('connected', () => {
+            console.log('Database connected successfully.');
+        })
+
+        mongoose.connection.on('disconnected', () => {
+            console.error('Mongoose connection disconnected.');
+        })
+
+        mongoose.connection.on('error', err => {
+            console.error(`Error occurred after connection ${err}`);
+        })
+
+        await mongoose.connect(url);
     } catch (error) {
         console.log(error);
         process.exit(1);
