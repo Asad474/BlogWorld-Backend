5499f5989ba554d88c5f1d8b15e29db4f80681e4
Asad Sarfaraz <asadsarfraz474@gmail.com>
2024-09-27 02:05:53 +0530

    Verifying jwt token asynchronously

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,29 +1,40 @@
 import { NextFunction, Response } from "express";
-import jwt, { JwtPayload } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import { User } from "../models";
 import { UnauthorizedError } from "../utils";
 import { ExtendRequest } from "../interfaces";
+
+const jwtVerify = (token: string, secret: string): Promise<any> => {
+    return new Promise((resolve, reject) => {
+        jwt.verify(token, secret, (err, decoded) => {
+            if (err) 
+                return reject(err);
+            
+            resolve(decoded);
+        });
+    });
+}
 
 export const protect = async(req: ExtendRequest, res: Response, next: NextFunction) => {
     try {
         const token = req.cookies?.jwt_token;
 
-        if (token) {
-            try {
-                const decoded = jwt.verify(token, process.env.JWT_TOKEN as string) as JwtPayload;
-                const user = await User.findById(decoded._id).select('-password').lean();
-
-                if (!user){
-                    throw new UnauthorizedError('User does not exists.');
-                }
-
-                req.user = user;
-                next();
-            } catch (error) {
-                throw new UnauthorizedError('Unauthorized, invalid token');
-            }
-        } else{
+        if (!token) {
             throw new UnauthorizedError('Unauthorized, no token');
+        }
+        
+        try {
+            const decoded = await jwtVerify(token, process.env.JWT_TOKEN as string);
+            const user = await User.findById(decoded._id).select('-password').lean();
+
+            if (!user){
+                throw new UnauthorizedError('User does not exists.');
+            }
+
+            req.user = user;
+            next();
+        } catch (error) {
+            throw new UnauthorizedError('Unauthorized, invalid token');
         }
     } catch (error) {
         console.log(error);
